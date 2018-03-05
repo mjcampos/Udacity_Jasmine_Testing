e9f22b91588720406c6115ba174a0f4f80cd0543
Michael Campos <mjcampos@Michaels-MacBook-Pro.local>
2018-03-04 23:26:41 -0800

    Setup 'RSS Feeds' testing
    Made revision to terminal display of user email

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,17 +31,29 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('have URLs that are defined and with value', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            });
+         });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('have names that are defined and with value', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            });
+         });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe("The menu", function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
@@ -53,6 +65,7 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
